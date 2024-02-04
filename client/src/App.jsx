6f2f8d4a5e94f2f2676abe2d9df0908f24e78764
longquan98@gmail.com <longquan98@gmail.com>
2024-02-03 23:12:55 -0500

    Handle routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Courses from "./components/Courses";
 import Student from "./components/Students";
 
 function App() {
   return (
-    <>
-      <Courses />
-      <Student />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/students" element={<Student />} />
+      </Routes>
+    </Router>
   );
 }
 
