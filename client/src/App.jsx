5fad9c6fc213bda455ad2c1aa59b516ccc8d45cd
longquan98@gmail.com <longquan98@gmail.com>
2024-02-03 22:51:41 -0500

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Courses from "./components/Courses";
+
+function App() {
+  return (
+    <>
+      <Courses />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Courses.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+
+const Courses = () => {
+  const coursesfromDB = [
+    { id: 1, title: "Course 1" },
+    { id: 2, title: "Course 2" },
+    { id: 3, title: "Course 3" },
+  ];
+
+  const [courses, setCourses] = useState(coursesfromDB);
+
+  const addCourse = () => {
+    setCourses([...courses, { id: 4, title: "Course 4" }]);
+  };
+
+  const deleteCourse = (id) => {
+    const newCourses = courses.filter((course) => course.id !== id);
+    setCourses(newCourses);
+  };
+
+  const updateCourse = (id) => {
+    const newCourses = courses.filter((course) => course.id !== id);
+    setCourses(newCourses);
+  };
+
+  return (
+    <div>
+      {courses.map((course) => (
+        <div key={courses.id}>
+          {course.title}{" "}
+          <button onClick={() => deleteCourse(course.id)}>Delete</button>
+          <button onClick={() => updateCourse(course.id)}>Update</button>
+        </div>
+      ))}
+      <button onClick={addCourse}>Add</button>
+    </div>
+  );
+};
+
+export default Courses;
