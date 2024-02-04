fab8c374ba4c0a50206208baeef2fbd3eb4ed645
longquan98@gmail.com <longquan98@gmail.com>
2024-02-03 23:07:55 -0500

    Create Students component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import Courses from "./components/Courses";
+import Student from "./components/Students";
 
 function App() {
   return (
     <>
       <Courses />
+      <Student />
     </>
   );
 }
---
+++ client/src/components/Students.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Students = () => {
+  const students = [
+    {
+      id: 1,
+      name: "Alice",
+    },
+    {
+      id: 2,
+      name: "Bob",
+    },
+    {
+      id: 3,
+      name: "Charlie",
+    },
+  ];
+
+  return (
+    <div>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>{student.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Students;
