ad0d9467f329c1161ef8cf2f15352cc2ee47a66a
longquan98@gmail.com <longquan98@gmail.com>
2024-02-04 21:17:52 -0500

    Build the Course table to render all courses

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -2,41 +2,42 @@ import React from "react";
 import { useState } from "react";
 
 const Courses = () => {
-  const coursesfromDB = [
+  const courses = [
     { id: 1, title: "Course 1" },
     { id: 2, title: "Course 2" },
     { id: 3, title: "Course 3" },
   ];
 
-  const [courses, setCourses] = useState(coursesfromDB);
-
-  const addCourse = () => {
-    setCourses([...courses, { id: 4, title: "Course 4" }]);
-  };
-
-  const deleteCourse = (id) => {
-    const newCourses = courses.filter((course) => course.id !== id);
-    setCourses(newCourses);
-  };
-
-  const updateCourse = (id) => {
-    const newCourses = courses.filter((course) => course.id !== id);
-    setCourses(newCourses);
-  };
-
   return (
-    <div>
-      {courses.map((course) => (
-        <div key={course.id}>
-          {course.title}
-          <button onClick={() => deleteCourse(course.id)}>Delete</button>
-          <button onClick={() => updateCourse(course.id)}>Update</button>
-        </div>
-      ))}
-      <button>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Course Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr
+              key={course.id}
+              className="d-flex flex-row justify-content-between"
+            >
+              <td>{course.title}</td>
+              <td>
+                <div>
+                  <button className="btn btn-danger mx-3">Delete</button>
+                  <button className="btn btn-success">Update</button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button className="btn btn-secondary">
         <a href="/new-course">Add Course</a>
       </button>
-    </div>
+    </>
   );
 };
 
